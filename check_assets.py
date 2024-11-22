f85ca179aad5ce63833ae2659a39732be32010ed
Mickael <mickael.shonouda@etu.ec-lyon.fr>
2024-11-22 08:16:49 +0100

    fix tests

--- check_assets.py
+++ check_assets.py
@@ -6,8 +6,12 @@ with open("assos_links.yaml", "r", encoding="utf8") as links_file:
     data = yaml.load(links_file, Loader=yaml.CLoader)
 
 for banner in data:
+    if not exists("src/assets/icons/" + banner["icon"]):
+            raise ValueError(
+                f"File {banner['icon']} is missing from the assets directory"
+            )
     for link in banner["links"]:
-        if not exists("src/assets/icons/" + link["icon"]):
+        if not exists("src/assets/svg_icons/" + link["icon"]):
             raise ValueError(
                 f"File {link['icon']} is missing from the assets directory"
             )
