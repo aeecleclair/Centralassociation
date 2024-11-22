a9872d9289edec4806bad62736b06ddbac325f4d
Mickael <mickael.shonouda@etu.ec-lyon.fr>
2024-11-22 08:12:43 +0100

    Update check_assets.py

--- check_assets.py
+++ check_assets.py
@@ -5,8 +5,8 @@ import yaml
 with open("assos_links.yaml", "r", encoding="utf8") as links_file:
     data = yaml.load(links_file, Loader=yaml.CLoader)
 
-for category_name in data:
-    for link in data[category_name]:
+for banner in data:
+    for link in banner["links"]:
         if not exists("src/assets/icons/" + link["icon"]):
             raise ValueError(
                 f"File {link['icon']} is missing from the assets directory"
